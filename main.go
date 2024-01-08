f1ad6e925319f27b33fdc0090c15cdeb37d7e18e
supremo4031 <layekarpan30@gmail.com>
2024-01-09 00:05:21 +0530

    add basic quiz game
    add basic quiz game

--- main.go
+++ main.go
@@ -1,6 +1,65 @@
 package main
 
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
 
 func main() {
+	// create flags for getting the filename (flag package)
+	csvFileName := flag.String("csv", "problems.csv", "a csv file in the format of 'question, answer'")
+	flag.Parse()
 	
+	// load the file (os package)
+	file, err := os.Open(*csvFileName)
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open the csv file: %s\n", *csvFileName))
+	}
+
+	// read the file as csv (csv package)
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("Failed to parse the provided CSV file")
+	}
+
+	problems := parseLines(lines)
+
+	correct := 0
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = \n", i + 1, p.q)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == p.a {
+			correct++
+		}
+	}
+
+	fmt.Printf("You scored %d out of %d\n", correct, len(problems))
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem{
+			q: line[0],
+			// trim the leading or trailing spaces (strings package)
+			a: strings.TrimSpace(line[1]),
+		}
+	}
+	return ret
+}
+
+type problem struct {
+	q string
+	a string
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
 }
